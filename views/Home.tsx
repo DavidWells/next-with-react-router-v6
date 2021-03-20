69951e5eff1ec1fefd527d99b87c39a0116bd986
DavidWells <>
2021-03-20 16:14:37 -0700

    insta load home

--- views/Home.tsx
+++ views/Home.tsx
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 export default function Home() {
-	const [loaded, setLoaded] = useState(false)
-
-	useEffect(() => setLoaded(true), [])
-
-	let homePage = <div>Home loading...</div>
-	if (loaded) {
-		homePage = <h1>Home Page</h1>
-	}
 	return (
-		<div id='home'>
-			{homePage}
+		<div>
+			<h1>Home Page</h1>
 		</div>
 	)
 }
