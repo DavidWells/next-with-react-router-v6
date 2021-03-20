cabeacfce9105fb1068a2849bc6d00d69bb2ce2b
DavidWells <>
2021-03-20 14:03:33 -0700

    add normal links

--- views/Navigation.tsx
+++ views/Navigation.tsx
@@ -3,61 +3,90 @@ import { Link } from 'react-router-dom'
 import NextLink from 'next/link'
 
 const routes = [
-	{ href: '/',
+	{ 
+		href: '/',
 		type: 'react-router',
 	},
-	{ href: '/users',
+	{ 
+		href: '/users',
 		type: 'react-router',
 	},
-	{ href: '/users/12345-xyz',
+	{ 
+		href: '/users/12345-xyz',
 		type: 'react-router',
 	},
-	{ href: '/users/me',
+	{ 
+		href: '/users/me',
 		type: 'react-router',
 	},
-	{ href: '/other',
+	{ 
+		href: '/other',
 		type: 'react-router',
 	},
-	{ href: '/foo',
+	{ 
+		href: '/foo',
 		type: 'react-router',
 	},
-	{ href: '/foo/bar',
+	{ 
+		href: '/foo/bar',
 		type: 'react-router',
 	},
-	{ href: '/get-static-props',
+	{ 
+		href: '/get-static-props',
 		type: 'next',
 	},
-	{ href: '/get-server-side-props',
+	{ 
+		href: '/get-server-side-props',
 		type: 'next',
 	},
-	{ href: '/about',
+	{ 
+		href: '/about',
 		type: 'next',
 	},
-	{ href: '/fake-page-to-404',
+	{ 
+		href: '/fake-page-to-404',
 		type: 'react-router',
-	}
+	},
+	{ 
+		href: '/api/example',
+		type: 'next-api',
+	},
+	{ 
+		href: '/.netlify/functions/hello',
+		type: 'netlify-function',
+	},
 ]
 
 export default function Navigation() {
 	return (
 		<section style={{ display: 'flex' }}>
 			<div style={{ marginRight: 70 }}>
-				<h2>Using React Router "Link" component</h2>
+				<h2>Using React Router &lt;Link&gt;</h2>
 				{routes.map(({ href, type }) => (
-					<div>
-						<Link key={href} to={href}>
+					<div key={href}>
+						<Link to={href}>
 							<span>{href} - ({type}) - <i>&lt;Link&gt;</i></span>
 						</Link>
 					</div>
 				))}
 			</div>
-			<div>
-				<h2>Using NextJS "Link" component</h2>
+			<div style={{ marginRight: 70 }}>
+				<h2>Using NextJS &lt;NextLink&gt;</h2>
 				{routes.map(({ href, type }) => (
-					<div>
-						<NextLink key={href} href={href}>
+					<div key={href}>
+						<NextLink href={href}>
 							<a>{href} - ({type}) - <i>&lt;NextLink&gt;</i></a>
 						</NextLink>
+					</div>
+				))}
+			</div>
+			<div>
+				<h2>Using Normal &lt;a&gt; tag</h2>
+				{routes.map(({ href, type }) => (
+					<div key={href}>
+						<a href={href}>
+							{href} - ({type}) - <i>&lt;a&gt;</i>
+						</a>
 					</div>
 				))}
 			</div>
