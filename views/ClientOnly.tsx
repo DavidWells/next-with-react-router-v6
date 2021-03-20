e4f20716d616ce804c73758afe95404b48b38a05
DavidWells <>
2021-03-20 15:32:24 -0700

    add client only

--- views/ClientOnly.tsx
+++ views/ClientOnly.tsx
@@ -2,9 +2,9 @@ import * as React from 'react'
 
 export default function ClientOnly() {
 	return (
-		<section>
+		<div>
 			<h1>Client only page</h1>
 			<p>This page is not statically generated on build and only lives in react bundle</p>
-		</section>
+		</div>
 	)
 }
